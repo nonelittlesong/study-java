431730239a741f53f78fbde2a5aa98dbce724180
汲蛤扑 <song_csu@outlook.com>
2018-12-05 15:59:31 +0800

    Update LogHelper.java

--- Log4j/LogHelper.java
+++ Log4j/LogHelper.java
@@ -100,3 +100,75 @@ public static void fileCopy(File in, File out) throws IOException {
     }
 }
 
+/**
+ * 8. 创建图片缩略图
+ */
+private void createThumbnail(String filename, int thumbWidth, int thumbHeight, int quality, String outFilename)
+        throws InterruptException, FileNotFoundException, IOException {
+    // load image from filename
+    Image image = Toolkit.getDefaultToolkit().getImage(filename);
+    MediaTracker mediaTracker = new MediaTracker(new Container());
+    mediaTracker.addImage(image, 0);
+    mediaTracker.waitForID(0);
+    // use this to test for errors at this point:
+    // System.out.println(mediaTracker.isErrorAny());
+    
+    // determine thumbnail size from WIDTH and HEIGHT
+    double thumbRatio = (double)thumbWidth / (double)thumbHeight;
+    int imageWidth = image.getWidth(null);
+    int imageHeight = image.getHeight(null);
+    double imageRatio = (double)imageWidth / (double)imageHeight;
+    if (thumbRatio < imageRatio) {
+        thumbHeight = (int)(thumbWidth / imageRatio);
+    } else {
+        thumbWidth = (int)(thumbHeight * imageRatio);
+    }
+    
+    // draw original image to thumbnail image object and
+    // scale it to the new size on-the-fly
+    BufferedImage thumbImage = new BufferedImage(thumbWidth, thumbHeight, BufferedImage.TYPE_INT_RGB);
+    Graphics2D graphics2D = thumbImage.createGraphics();
+    graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+    graphics2D.drawImage(image, 0, 0, thumbWidth, thumbHeight, null);
+    
+    // save thumbnail image to outFilename
+    BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(outFilename));
+    JPEGImageEncoder encoder = JPEGCodec.createJPEGEnocder(out);
+    JPEGEncodeParam param = encoder.getDefaultJPEGEncoderParam(thumbImage);
+    quality = Math.max(0, Math.min(quality, 100));
+    param.setQuality((float)quality / 100.0f, false);
+    encoder.setJPEGEncodeParam(param);
+    encdoer.encode(thumbImage);
+    out.close();
+}
+
+/**
+ * 9. 创建JSON（json-rpc-1.0.jar）
+ */
+import org.json.JSONObject;
+...
+...
+JSONObject json = new JSONObject();
+json.put("city", "Mumbai");
+json.put("country", "India");
+...
+String output = json.toString();
+...
+    
+/**
+ * 10. 使用iTextJAR生成PDF
+ */
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class GeneratePDF {
+    public static void main(String[] args) {
+        OutputStream file = new FileOutputStream(new File("C:\\Test.pdf"));
+        Document document = new Document();
+        PdfWriter.getInstance

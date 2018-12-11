8c8960a410743085326a6306b5f577a8a7d77bc6
汲蛤扑 <song_csu@outlook.com>
2018-12-11 08:58:12 +0800

    Create TabLayout.java

---
+++ Layout/TabLayout.java
@@ -0,0 +1,5 @@
+    private void setupTabLayout(TabLayout tabLayout) {
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
